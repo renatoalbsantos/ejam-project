aaccee6c2bae04510874d83bac5f2ac7ca6d2bfb
renatoalbsantos <88661269+renatoalbsantos@users.noreply.github.com>
2023-09-21 08:58:23 -0300

    first commit

---
+++ src/pages/Main.js
@@ -0,0 +1,19 @@
+import '../style/main/order-page.scss';
+import Grid from "@material-ui/core/Grid";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import McAfee from ""
+
+function Main() {
+  return (
+       <Grid>
+            <Header/>
+           <Grid id={"order-page"}>
+
+           </Grid>
+           <Footer/>
+       </Grid>
+  );
+}
+
+export default Main;
